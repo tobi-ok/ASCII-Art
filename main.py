05c661ce8cf31b7bdecc731b5d1ad8a2617e25f5
tobi-ok <tobiok.cr@gmail.com>
2024-02-10 14:09:55 -0500

    Optimizations + bug fixes

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from PIL import Image
 
 image_path = 'your_file_path'
+brightness_mode = 'average'
+ascii_characters = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 
-im = Image.open(image_path)
-
-pixel_matrix = []
-pixel_flat = []
-ascii_charcters = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
-ascii_matrix = [[] for _ in range(im.height)] #NOTE: iterating through width will flip and rotate image
+def get_image_object(path):
+    return Image.open(path)
 
 def rgb_to_averagebrightness(r, g, b):
     return int((r + g + b) / 3)
@@ -15,45 +13,87 @@ def rgb_to_averagebrightness(r, g, b):
 def rgb_to_luminance(r, g, b):
     return int(((r * 299)+ (g * 587)+ (b * 114)) / 1000)
 
-# Loop over each pixel and append pixel data to list 
-for x in range(im.width):
-    row = []
-    pixel_matrix.append(row)
+def get_pixel_matrix(im):
+    ''' Returns every pixel in an image in matrix form '''
+    pixel_matrix = []
 
+    # Loop over each pixel and append pixel data to list 
     for y in range(im.height):
-        pixel_rgb = im.getpixel((x, y))
-        row.append(pixel_rgb)
-        pixel_flat.append(pixel_rgb)
+        row = []
 
-'''
-c = 20
-b = 0-100
+        for x in range(im.width):
+            pixel_rgb = im.getpixel((x, y))
+            row.append(pixel_rgb)
 
-b[50] = c[10]
-b[60] = c[12]
-b[30] = c[6]
-b[75] = c[15]
-b[20] = c[4]
+        pixel_matrix.append(row)
+    
+    return pixel_matrix
 
-y = 1/5x
+def get_pixelbrightness_matrix(pixel_matrix):
+    ''' Returns pixel brightness from a given pixel matrix '''
 
-255/65 = ~1/4x
-'''
+    width = len(pixel_matrix[1])
+    height = 0
 
-# Loop over each stored pixel data and insert to new list based on position
-for x in range(im.width):
-    for y in range(im.height):
-        pixel = pixel_matrix[x][y]
-        brightness = rgb_to_averagebrightness(pixel[0], pixel[1], pixel[2])
-        ascii_matrix[y].insert(x, ascii_charcters[int(brightness * 1/4)]) #NOTE: table index and insert index MUST match ascii matrix
+    for _ in pixel_matrix:
+        height += 1
 
-picture = ''
+    brightness_matrix = [[] for _ in range(height-1)]
 
-# Scale results
-for x in ascii_matrix:
-    for y in x:
-        picture += y*2
+    # Loop over each stored pixel data and insert to new list based on position
+    for x in range(width-1):
+        for y in range(height-1):
+            pixel_rgb = pixel_matrix[y][x]
+            brightness = None
+            
+            if brightness_mode.lower() == 'average':
+                brightness = rgb_to_averagebrightness(pixel_rgb[0], pixel_rgb[1], pixel_rgb[2])
+            elif brightness_mode.lower() == 'luminance':
+                brightness = rgb_to_luminance(pixel_rgb[0], pixel_rgb[1], pixel_rgb[2])
 
-    picture += '\n'
+            brightness_matrix[y].insert(x, brightness)
 
-print(picture)
+    return brightness_matrix
+
+def get_ascii_matrix(brightness_matrix):
+    ''' Returns char matrix from given brightness matrix 
+    c = 20
+    b = 0-100
+
+    b[50] = c[10]
+    b[60] = c[12]
+    b[30] = c[6]
+    b[75] = c[15]
+    b[20] = c[4]
+
+    y = 1/5x
+
+    255/65 = ~1/4x
+    '''
+
+    ascii_matrix = list(brightness_matrix)
+
+    for y in brightness_matrix:
+        for x in y:
+            ascii_matrix[y][x] = ascii_characters[int(x * 1/4)]
+
+    return ascii_matrix
+
+def pixelmatrix_to_picture(ascii_matrix):
+    picture = ''
+
+    # Scale results
+    for y in ascii_matrix:
+        for x in y:
+            picture += x*2
+
+        picture += '\n'
+
+    return picture
+
+if __name__ == '__main__':
+    im = get_image_object(image_path)
+    pixel_matrix = get_pixel_matrix(im)
+    brightness_matrix = get_pixelbrightness_matrix(pixel_matrix)
+    ascii_matrix = get_ascii_matrix(brightness_matrix)
+    print(pixelmatrix_to_picture(ascii_matrix))
